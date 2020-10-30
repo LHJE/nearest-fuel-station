c95ef7b356c79b22a74b16ee518f21751cdc0ea3
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:46:23 -0600

    Update direction with directions

--- app/poros/direction.rb
+++ app/poros/direction.rb
@@ -1,3 +1,15 @@
 class Direction
+  attr_reader :directions
 
+  def initialize(attrs)
+    @directions = get_directions(attrs)
+  end
+
+  def get_directions(attrs)
+    narrative = []
+    attrs.each do |attr|
+      narrative << attr[:narrative]
+    end
+    narrative
+  end
 end
