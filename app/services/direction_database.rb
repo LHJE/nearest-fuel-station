5e5a717c70ebf125a48e7009a6b16d8efa31aa22
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:41:37 -0600

    Update direction database method to make API call

--- app/services/direction_database.rb
+++ app/services/direction_database.rb
@@ -1,3 +1,16 @@
 class DirectionDatabase
+  def self.get_station(starting_location, ending_location)
+    key = ENV['MAPQUEST_API_KEY']
+    url = 'http://www.mapquestapi.com/directions/v2/'
+    uri = "route?key=#{key}&from=#{starting_location}&to=#{ending_location}"
+    get_results(url, key, uri)
+  end
 
+  def self.get_results(url, key, uri)
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.headers['X-API-Key'] = key
+    end
+    results = conn.get(uri)
+    JSON.parse(results.body, symbolize_names: true)
+  end
 end
