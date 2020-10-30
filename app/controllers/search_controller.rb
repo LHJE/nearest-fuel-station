544f99bec0f14384b4fb292bd74bb1bb07b39951
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:40:54 -0600

    Update search controller so directions has both start and end addresses

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     @station = SearchFacade.find_station(params[:location])
-    @directions = SearchFacade.find_directions(params[:location])
+    @directions = SearchFacade.find_directions(params[:location], @station.street_address)
   end
 end
