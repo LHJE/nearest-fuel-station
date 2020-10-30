e89a8b69a5a0240f834da328aaaacc41fda559df
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 11:07:41 -0600

    Update direction poro to allow access to travel time

--- app/poros/direction.rb
+++ app/poros/direction.rb
@@ -1,8 +1,10 @@
 class Direction
-  attr_reader :directions
+  attr_reader :directions,
+              :travel_time
 
   def initialize(attrs)
     @directions = get_directions(attrs)
+    @travel_time = get_travel_time(attrs)
   end
 
   def get_directions(attrs)
@@ -11,5 +13,18 @@ class Direction
       narrative << attr[:narrative]
     end
     narrative
+  end
+
+  def get_travel_time(attrs)
+    hours = []
+    minutes = []
+    seconds = []
+    attrs.each do |attr|
+      hours << attr[:formattedTime].split(":")[0].to_i
+      minutes << attr[:formattedTime].split(":")[1].to_i
+      seconds << attr[:formattedTime].split(":")[2].to_i
+    end
+    time = (hours.sum * 360 + minutes.sum * 60 + seconds.sum)
+    Time.at(time).utc.strftime("%H:%M:%S")
   end
 end
