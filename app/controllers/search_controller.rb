523bd4ce09d92ce96c981f9ee34b5b072b8e4eba
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:32:35 -0600

    Update search controller with directions object

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
     @station = SearchFacade.find_station(params[:location])
+    @directions = SearchFacade.find_directions(params[:location])
   end
 end
