b6b9e0079fad19f116861a6a562b6cfbedbc7e6c
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 09:28:43 -0600

    Update search spec with expect path test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 
 RSpec.describe 'Navbar' do
+  before :each do
+    visit root_path
 
-  
+    click_button "Find Nearest Station"
+  end
+
+  it "expects path to lead to search page" do
+    expect(current_path).to eq(search_path)
+  end
+
 end
