b1a37b5b8db3a0cc7a65bb867a0ec3a0b40acad7
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:27:00 -0600

    Update station to provide distance as an attribute

--- app/poros/station.rb
+++ app/poros/station.rb
@@ -2,13 +2,15 @@ class Station
   attr_reader :station_name,
               :street_address,
               :fuel_type,
-              :access_times
+              :access_times,
+              :distance
 
   def initialize(attrs)
     @station_name = attrs[:station_name]
     @street_address = get_address(attrs[:street_address], attrs[:city], attrs[:state], attrs[:zip])
     @fuel_type = get_fuel_type(attrs[:fuel_type_code])
     @access_times = attrs[:access_days_time].split(";")
+    @distance = attrs[:distance].round(1)
   end
 
   def get_address(street, city, state, zip)
@@ -18,6 +20,8 @@ class Station
   def get_fuel_type(type)
     if type == "ELEC"
       "Electric"
+    else
+      "Not Electric"
     end
   end
 end
