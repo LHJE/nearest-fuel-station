cffbc8f48782681127033c5a064b7fb017467e67
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:05:58 -0600

    Update search facade to get more relevent information to the stationporo

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,6 +1,6 @@
 class SearchFacade
   def self.find_station(starting_location)
-    attributes = StationDatabase.location(starting_location)
+    attributes = StationDatabase.get_station(starting_location)[:fuel_stations][0]
     Station.new(attributes)
   end
 end
