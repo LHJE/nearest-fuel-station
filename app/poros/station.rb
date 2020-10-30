f4dffef5d371370b1f0b2538f97b6e39654d24e7
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-10-30 10:05:33 -0600

    Update station poro with intialize and att_reader

--- app/poros/station.rb
+++ app/poros/station.rb
@@ -1,3 +1,14 @@
 class Station
+  attr_reader :name,
+              :address,
+              :fuel_type,
+              :access_times
 
+  def initialize(attrs)
+    require "pry"; binding.pry
+    @name = attrs[:name]
+    @address = attrs[:address]
+    @fuel_type = attrs[:fuel_type]
+    @access_times = attrs[:access_times]
+  end
 end
